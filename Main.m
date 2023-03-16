38c264c43cd8662eeb272748cbf8d1e7db507a28
PedroSequeira156 <Pedrodfmsequeira@gmail.com>
2023-03-16 12:10:48 +0000

    1 - updating the code to Finn's version

--- Main.m
+++ Main.m
@@ -1,8 +1,6 @@
 clear all 
 close all
 
-% change
-%aaaaaaaaaaaaaaaaa
 %% Read Data %%
 
 Propellant_Data = readtable('Data.xlsx','Sheet','Propellant Data');
@@ -11,13 +9,10 @@ Engine_Data = readtable('Data.xlsx','Sheet','Engine Data');
 %% Initial Generations %%
 n_Stage = 3;
 
-% randomising the choice of initial generation
-%x0 = randi([1,height(Engine_Data)],1,n_Stage);
+x0 = randi([1,18],5,n_Stage);
 
-% setting an initial generation (to make sure everything is good)]
-x0 = [14,14,14];
 %% Functions %%
 
-[Mass_Stage,Prop_Stage, Mass_Total] = Mass(Engine_Data,x0);
-[Delta_V, Delta_V_tot] = Propulsion(Engine_Data,Mass_Stage,Prop_Stage, Mass_Total,x0,n_Stage);
+[Mass_Stage, Mass_Total] = Mass(Engine_Data,x0);
+[Delta_V, Delta_V_tot] = Propulsion(Engine_Data,Mass_Stage,Mass_Total,x0,n_Stage);
 [Cost_Stage, Cost_Total] = Finance(Mass_Stage,n_Stage);
--- Propulsion.m
+++ Propulsion.m
@@ -1,15 +1,24 @@
-function [Delta_V, Delta_V_tot] = Propulsion(Engine_Data,Mass_Stage,Mass_Fuel, Mass_Total,x0,n_Stage)
+function [Delta_V, Delta_V_tot] = Propulsion(Engine_Data,Mass_Stage,Mass_Total,x0,n_Stage)
 
+%% Inputs %%
+
+FR_Max = 0.95;
+k1 = [0.5917 0.4917 0.3917 0.2917];
+k2 = 0.29;
+g = 9.81;
+lb_2_Kg = 0.453592;
+Kg_2_lb =2.20462;
+
+%% Mass Fraction Per Stage %%
+
+FR = FR_Max - k1(1:n_Stage).*((Mass_Stage.*Kg_2_lb).^(-k2));
+Mass_Fuel = FR.*Mass_Stage;
 
 %% Delta V %%
 
-Delta_V = zeros(n_Stage,1);
-
 for n = 1:n_Stage
-    % calculating the deltaV of this stage
-    Delta_V(n) = Engine_Data.ISP(x0(n)).*log(Mass_Total./(Mass_Total - Mass_Fuel(:,n)));
-    % removing the stage
-    Mass_Total = Mass_Total - Mass_Stage(n);
+    Delta_V(:,n) = (Engine_Data.ISP(x0(:,n)).*g).*log(Mass_Total(:,1)./(Mass_Total(:,1) - Mass_Fuel(:,n)));
+    Mass_Total = Mass_Total - Mass_Stage(:,n);
 end
 
-Delta_V_tot = sum(Delta_V);
+Delta_V_tot = sum(Delta_V,2);
