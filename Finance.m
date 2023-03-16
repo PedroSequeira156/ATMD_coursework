4dbc293e802036eb2f55b9b2eb6f1243ad436813
PedroSequeira156 <Pedrodfmsequeira@gmail.com>
2023-03-16 18:05:33 +0000

    2 - Solving DeltaV problem
    The changes are displayed with comments in the files

--- Finance.m
+++ Finance.m
@@ -1,4 +1,4 @@
-function [Cost_Stage, Cost_Total] = Finance(Mass_Stage,n_Stage)
+function [Cost_Stage, Cost_Total] = Finance(Mass_Stage_Empty,n_Stage)
 
 %% Inputs %%
 
@@ -7,7 +7,7 @@ k4 = 0.253*(10^(-5));
 C_min = 1;
 lb_2_Kg = 0.453592;
 Kg_2_lb =2.20462;
-Mass_Stage_lb = Mass_Stage.*Kg_2_lb;
+Mass_Stage_lb = Mass_Stage_Empty.*Kg_2_lb;
 %% Cost %%
 
 Cost_Stage = (C_min + k3(1:n_Stage).*exp(-k4.*Mass_Stage_lb)).*Mass_Stage_lb;
--- Propulsion.m
+++ Propulsion.m
@@ -1,24 +1,19 @@
-function [Delta_V, Delta_V_tot] = Propulsion(Engine_Data,Mass_Stage,Mass_Total,x0,n_Stage)
+function [Delta_V, Delta_V_tot] = Propulsion(Engine_Data,Mass_Stage_Empty,Mass_Stage_Fuel,Mass_Total,x0,n_Stage)
 
-%% Inputs %%
 
-FR_Max = 0.95;
-k1 = [0.5917 0.4917 0.3917 0.2917];
-k2 = 0.29;
+% constants
 g = 9.81;
-lb_2_Kg = 0.453592;
-Kg_2_lb =2.20462;
 
-%% Mass Fraction Per Stage %%
 
-FR = FR_Max - k1(1:n_Stage).*((Mass_Stage.*Kg_2_lb).^(-k2));
-Mass_Fuel = FR.*Mass_Stage;
+
+% I moved the section of calculating the propellent mass to the mass func
+
 
 %% Delta V %%
 
 for n = 1:n_Stage
-    Delta_V(:,n) = (Engine_Data.ISP(x0(:,n)).*g).*log(Mass_Total(:,1)./(Mass_Total(:,1) - Mass_Fuel(:,n)));
-    Mass_Total = Mass_Total - Mass_Stage(:,n);
+    Delta_V(:,n) = (Engine_Data.ISP(x0(:,n)).*g).*log(Mass_Total(:,1)./(Mass_Total(:,1) - Mass_Stage_Fuel(:,n)));
+    Mass_Total = Mass_Total - Mass_Stage_Empty(:,n)- Mass_Stage_Fuel(:,n);
 end
 
 Delta_V_tot = sum(Delta_V,2);
