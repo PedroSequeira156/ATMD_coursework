44cb95bffe27338541763d0b2d832a603da68ebb
Atlas-111 <antonio.seara.cardoso@gmail.com>
2023-03-16 12:00:07 +0000

    1

--- Mass.m
+++ Mass.m
@@ -1,20 +1,7 @@
-function [Mass_Stage,Prop_Stage, Mass_Total] = Mass(Engine_Data, x0)
+function [Mass_Stage, Mass_Total] = Mass(Engine_Data, x0)
 
+%% Code %%
 Mass_Payload = 1250;
 
-Mass_Stage_engine  = Engine_Data.Mass(x0);
-
-
-%% empirical constants
-Sigma_max   = 0.95;
-k2          = 0.29;
-k1          = [0.5917, 0.4917, 0.3917, 0.2917];
-
-%% Mass Fraction Per Stage %%
-Prop_mass_frac = Sigma_max - ((Mass_Stage_engine).^(-k2)').*k1(1:length(x0));
-Prop_Stage = Prop_mass_frac.*(Mass_Stage_engine');
-
-Mass_Stage = Mass_Stage_engine' + Prop_Stage;
-
-%% Total mass
-Mass_Total = sum(Mass_Stage) + Mass_Payload;
+Mass_Stage = Engine_Data.Mass(x0);
+Mass_Total = sum(Mass_Stage,2) + Mass_Payload;
--- Propulsion.m
+++ Propulsion.m
@@ -1,15 +1,24 @@
-function [Delta_V, Delta_V_tot] = Propulsion(Engine_Data,Mass_Stage,Mass_Fuel, Mass_Total,x0,n_Stage)
+function [Delta_V, Delta_V_tot] = Propulsion(Engine_Data,Mass_Stage,Mass_Total,x0,n_Stage)
 
+%% Inputs %%
+
+FR_Max = 0.95;                              %max propellant fraction
+k1 = [0.5917 0.4917 0.3917 0.2917];         %constant straight from paper
+k2 = 0.29;                                  %constant straight from paper
+g = 9.81;                                   %gravity constant on earth
+%lb_2_Kg = 0.453592;                        %pounds mass to kg
+Kg_2_lb =2.20462;                           %kg to pounds mass
+
+%% Mass Fraction Per Stage %%
+
+FR = FR_Max - k1(1:n_Stage).*((Mass_Stage.*Kg_2_lb).^(-k2));
+Mass_Fuel = FR.*Mass_Stage;
 
 %% Delta V %%
 
-Delta_V = zeros(n_Stage,1);
-
 for n = 1:n_Stage
-    % calculating the deltaV of this stage
-    Delta_V(n) = Engine_Data.ISP(x0(n)).*log(Mass_Total./(Mass_Total - Mass_Fuel(:,n)));
-    % removing the stage
-    Mass_Total = Mass_Total - Mass_Stage(n);
+    Delta_V(:,n) = (Engine_Data.ISP(x0(:,n)).*g).*log(Mass_Total(:,1)./(Mass_Total(:,1) - Mass_Fuel(:,n)));
+    Mass_Total = Mass_Total - Mass_Stage(:,n);
 end
 
-Delta_V_tot = sum(Delta_V);
+Delta_V_tot = sum(Delta_V,2);
